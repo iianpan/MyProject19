89f5010c05b4d8411a530bef52c9c2cc6626b3ab
iianpan <58591698+iianpan@users.noreply.github.com>
2020-04-26 16:32:17 +0800

    Initial commit

---
+++ Source/MyProject19/MyProject19GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyProject19GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT19_API AMyProject19GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
